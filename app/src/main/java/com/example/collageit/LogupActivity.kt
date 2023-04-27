302ec70117c1ac81468eec382ae39fb5cb1c794c
Justin Albert <Jnalbert879@gmail.com>
2023-04-27 09:19:37 -0700

    first commit don't mess things up

---
+++ app/src/main/java/com/example/collageit/LogupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.collageit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logup)
+    }
+}
