1768d2d123bcdc0a9a1dabb10d7795486abc5398
FawnKitten <joaopollo07@gmail.com>
2023-05-11 10:06:37 -0700

    !!BROKEN!! Upload multiple images

--- app/src/main/java/com/example/collageit/ImageUploadActivity.kt
+++ app/src/main/java/com/example/collageit/ImageUploadActivity.kt
@@ -1,5 +1,6 @@
 package com.example.collageit
 
+import android.content.Context
 import android.content.Intent
 import android.database.Cursor
 import android.net.Uri
@@ -10,12 +11,16 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.FileNotFoundException
 
+private fun <T> T.debug(): T {
+    Log.d(ImageUploadActivity.TAG, "DEBUG: $this")
+    return this
+}
 
 class ImageUploadActivity : AppCompatActivity() {
 
     companion object {
-        val TAG = "ImageUploadActivity"
-        val PHOTO_REQUEST = 1
+        const val TAG = "ImageUploadActivity"
+        const val PHOTO_REQUEST = 1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +28,11 @@ class ImageUploadActivity : AppCompatActivity() {
         setContentView(R.layout.activity_image_upload)
 
         findViewById<TextView>(R.id.textView8).setOnClickListener {
-            val photoPickerIntent = Intent(Intent.ACTION_PICK)
-            photoPickerIntent.type = "image/*"
-            startActivityForResult(photoPickerIntent, PHOTO_REQUEST)
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            intent.type = "image/*"
+            startActivityForResult(intent, PHOTO_REQUEST)
         }
     }
 
@@ -33,14 +40,17 @@ class ImageUploadActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PHOTO_REQUEST) if (resultCode == RESULT_OK) {
-            val selectedImage: Uri? = data?.data
-            val filePath = getPath(selectedImage)
-            val file_extn = filePath.substring(filePath.lastIndexOf(".") + 1)
-            Log.d(TAG, "onActivityResult: $filePath")
+            Log.d(TAG, "onActivityResult: uri - ${data?.clipData}")
+            val clipData = data?.clipData!!.debug()
+            val fileUris = (0 until clipData.itemCount).map { clipData.getItemAt(it).uri }.debug()
+            val filePaths = fileUris.map { getRealPathFromURI(this, it.debug()) }
+            val fileExtns = filePaths.map { it.substring(it.lastIndexOf(".") + 1) }
+            Log.d(TAG, "onActivityResult: $filePaths")
             try {
-                if (file_extn == "img" || file_extn == "jpg" || file_extn == "jpeg" || file_extn == "gif" || file_extn == "png") {
+                val allowedFiletypes = listOf("img", "jpg", "jpeg", "png")
+                if (fileExtns.any { !allowedFiletypes.contains(it) }) {
                     //FINE
-                    Log.d(TAG, "onActivityResult: in the if with $filePath $file_extn")
+                    Log.d(TAG, "onActivityResult: in the if with $filePaths $fileExtns")
                 } else {
                     //NOT IN REQUIRED FORMAT
                     Log.d(TAG, "onActivityResult: not in required format")
@@ -52,13 +62,32 @@ class ImageUploadActivity : AppCompatActivity() {
         }
     }
 
-    fun getPath(uri: Uri?): String {
+    private fun getPath(uri: Uri?): String {
         val projection = arrayOf(MediaStore.MediaColumns.DATA)
         val cursor: Cursor = managedQuery(uri, projection, null, null, null)
-        var columnIndex = cursor
+        val columnIndex = cursor
             .getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
         cursor.moveToFirst()
-        var imagePath = cursor.getString(columnIndex)
-        return cursor.getString(columnIndex)
+        val imagePath = cursor.getString(columnIndex).debug()
+        return imagePath
+    }
+
+    fun getRealPathFromURI(context: Context, contentUri: Uri): String {
+        var cursor: Cursor? = null
+        return try {
+            val proj = arrayOf(MediaStore.Images.Media.DATA)
+            cursor = context.contentResolver.query(
+                contentUri, proj, null,
+                null, null
+            )
+            Log.d(TAG, "getRealPathFromURI: cursor - $cursor")
+            cursor!!
+            val column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            cursor.moveToFirst()
+            cursor.getString(column_index)
+        } finally {
+            cursor?.close()
+        }
     }
 }
