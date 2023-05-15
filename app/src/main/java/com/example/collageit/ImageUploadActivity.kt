bc11d1a10da9645d91e1966ff9c6285a9a073c4c
FawnKitten <joaopollo07@gmail.com>
2023-05-15 08:56:39 -0700

    Got URIS for images from gallery.
    Added note for justin to get the data and send it to the formatting activity

--- app/src/main/java/com/example/collageit/ImageUploadActivity.kt
+++ app/src/main/java/com/example/collageit/ImageUploadActivity.kt
@@ -1,15 +1,11 @@
 package com.example.collageit
 
-import android.content.Context
 import android.content.Intent
-import android.database.Cursor
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.io.FileNotFoundException
+import com.example.collageit.collageCreation.ChooseCollageFormatActivity
 
 private fun <T> T.debug(): T {
     Log.d(ImageUploadActivity.TAG, "DEBUG: $this")
@@ -42,52 +38,69 @@ class ImageUploadActivity : AppCompatActivity() {
         if (requestCode == PHOTO_REQUEST) if (resultCode == RESULT_OK) {
             Log.d(TAG, "onActivityResult: uri - ${data?.clipData}")
             val clipData = data?.clipData!!.debug()
+            // TODO: Only accept valid file extensions
+//            val filePaths = fileUris.map { getPath(it.debug()) }
+//            Log.d(TAG, "onActivityResult: filePaths - $filePaths")
+//            val fileExtns = filePaths.map { it.substring(it.lastIndexOf(".") + 1) }
+//            Log.d(TAG, "onActivityResult: $filePaths")
+//            try {
+//                val allowedFiletypes = listOf("img", "jpg", "jpeg", "png")
+//                if (fileExtns.any { !allowedFiletypes.contains(it) }) {
+//                    //FINE
+//                    Log.d(TAG, "onActivityResult: in the if with $filePaths $fileExtns")
+//                } else {
+//                    //NOT IN REQUIRED FORMAT
+//                    Log.d(TAG, "onActivityResult: not in required format")
+//                }
+//            } catch (e: FileNotFoundException) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace()
+//            }
+
+
+            // TODO: Send URIS to ChooseCollageFormatActivity
+            /***************************
+             *  NOTE TO JUSTIN         *
+             *  -------------          *
+             *                         *
+             *  INSERT EXTRAS HERE     *
+             *                         *
+             ***************************/
+
             val fileUris = (0 until clipData.itemCount).map { clipData.getItemAt(it).uri }.debug()
-            val filePaths = fileUris.map { getRealPathFromURI(this, it.debug()) }
-            val fileExtns = filePaths.map { it.substring(it.lastIndexOf(".") + 1) }
-            Log.d(TAG, "onActivityResult: $filePaths")
-            try {
-                val allowedFiletypes = listOf("img", "jpg", "jpeg", "png")
-                if (fileExtns.any { !allowedFiletypes.contains(it) }) {
-                    //FINE
-                    Log.d(TAG, "onActivityResult: in the if with $filePaths $fileExtns")
-                } else {
-                    //NOT IN REQUIRED FORMAT
-                    Log.d(TAG, "onActivityResult: not in required format")
-                }
-            } catch (e: FileNotFoundException) {
-                // TODO Auto-generated catch block
-                e.printStackTrace()
-            }
+            val intent = Intent(this, ChooseCollageFormatActivity::class.java)
+
         }
     }
 
-    private fun getPath(uri: Uri?): String {
-        val projection = arrayOf(MediaStore.MediaColumns.DATA)
-        val cursor: Cursor = managedQuery(uri, projection, null, null, null)
-        val columnIndex = cursor
-            .getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
-        cursor.moveToFirst()
-        val imagePath = cursor.getString(columnIndex).debug()
-        return imagePath
-    }
-
-    fun getRealPathFromURI(context: Context, contentUri: Uri): String {
-        var cursor: Cursor? = null
-        return try {
-            val proj = arrayOf(MediaStore.Images.Media.DATA)
-            cursor = context.contentResolver.query(
-                contentUri, proj, null,
-                null, null
-            )
-            Log.d(TAG, "getRealPathFromURI: cursor - $cursor")
-            cursor!!
-            val column_index = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-            cursor.moveToFirst()
-            cursor.getString(column_index)
-        } finally {
-            cursor?.close()
-        }
-    }
+//    // TODO Get file path
+//    private fun getPath(uri: Uri?): String {
+//        Log.d(TAG, "getPath: uri - $uri")
+//        val projection = arrayOf(MediaStore.MediaColumns.DATA)
+//        val cursor: Cursor = managedQuery(uri, projection, null, null, null)
+//        val columnIndex = cursor
+//            .getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
+//        cursor.moveToFirst()
+//        val imagePath = cursor.getString(columnIndex).debug()
+//        return imagePath
+//    }
+//
+//    fun getRealPathFromURI(context: Context, contentUri: Uri): String {
+//        var cursor: Cursor? = null
+//        return try {
+//            val proj = arrayOf(MediaStore.Images.Media.DATA)
+//            cursor = context.contentResolver.query(
+//                contentUri, proj, null,
+//                null, null
+//            )
+//            Log.d(TAG, "getRealPathFromURI: cursor - $cursor")
+//            cursor!!
+//            val columnIndex = cursor
+//                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+//            cursor.moveToFirst()
+//            cursor.getString(columnIndex)
+//        } finally {
+//            cursor?.close()
+//        }
+//    }
 }
