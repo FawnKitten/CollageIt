38f8d291c77ebe79ac445dd215858c4a902d8af5
austaciouscoder <davidchong1224@gmail.com>
2023-05-23 10:00:16 -0700

    a

--- app/src/main/java/com/example/collageit/CollageAdapter.kt
+++ app/src/main/java/com/example/collageit/CollageAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.collageit
 
 
+import android.annotation.SuppressLint
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.FirebaseApp
 import com.google.firebase.storage.FirebaseStorage
@@ -14,14 +17,22 @@ import com.squareup.picasso.Picasso
 
 
 class CollageAdapter (var dataSet: List<Collage>) : RecyclerView.Adapter<CollageAdapter.ViewHolder>(){
+
+    companion object {
+        const val EXTRA_TITLE = "EXTRA_TITLE"
+        const val EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION"
+    }
+    @SuppressLint("ResourceType")
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val textViewName: TextView
         val imageViewCollage: ImageView
+        val layout : ConstraintLayout
 
 
         init{
             textViewName = view.findViewById(R.id.textView_collageName_itemCollage)
             imageViewCollage = view.findViewById(R.id.imageView_collagePicture_itemCollage)
+            layout = view.findViewById(R.layout.activity_collagelistitem)
 
         }
     }
@@ -38,7 +49,9 @@ class CollageAdapter (var dataSet: List<Collage>) : RecyclerView.Adapter<Collage
 
         Picasso.get().load(collage.picture).into(holder.imageViewCollage);
 
+        holder.layout.setOnClickListener {
 
+        }
 
 
 
